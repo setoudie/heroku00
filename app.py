f455baeeca7d268da28de55642fe142fdd538866
setoudie <setoudie@gmail.com>
2024-07-25 01:42:25 +0000

    login

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!!'
+def home():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
