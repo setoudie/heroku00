37f51f40959e97aa3c2def38abec9d96e66e6a1e
setoudie <setoudie@gmail.com>
2024-07-27 02:01:10 +0000

    test 00 b

--- app.py
+++ app.py
@@ -13,19 +13,19 @@ def test():
     return "render_template('login.html')"
 
 
-@app.route('/test0')
-def test00():
-    db = get_db_connection()
-    curs = db.cursor()
-    curs.execute("""CREATE TABLE Customers (CustomerName varchar(255), ContactName varchar(255),FirstName varchar(255),Address varchar(255),City varchar(255));""")
-    curs.execute("""INSERT INTO Customers (CustomerName, ContactName, Address, City)
-VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger');""")
-    curs.execute("""SELECT * FROM Customers""")
-    data_test = curs.fetchall()
-    db.commit()
-    curs.close()
-    db.close()
-    return jsonify(msg=data_test)
+# @app.route('/test0')
+# def test00():
+#     db = get_db_connection()
+#     curs = db.cursor()
+#     curs.execute("""CREATE TABLE Customers (CustomerName varchar(255), ContactName varchar(255),FirstName varchar(255),Address varchar(255),City varchar(255));""")
+#     curs.execute("""INSERT INTO Customers (CustomerName, ContactName, Address, City)
+# VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger');""")
+#     curs.execute("""SELECT * FROM Customers""")
+#     data_test = curs.fetchall()
+#     db.commit()
+#     curs.close()
+#     db.close()
+#     return jsonify(msg=data_test)
 
 
 if __name__ == '__main__':
